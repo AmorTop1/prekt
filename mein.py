7d027f8ced412747392404b8a61e98e408493c2a
AmorTop1 <124185698+AmorTop1@users.noreply.github.com>
2024-02-04 19:18:00 +0200

    Update mein.py

--- mein.py
+++ mein.py
@@ -2,6 +2,7 @@ import yfinance as yf
 import pandas as pd
 import plotly.graph_objects as go
 
+
 class OrderBlocksIndicator:
     def __init__(self, ticker, candle_range=15, show_pd=False, show_bearish_bos=False, show_bullish_bos=False):
         self.ticker = ticker
@@ -13,7 +14,7 @@ class OrderBlocksIndicator:
         self.fig = go.Figure()
 
     def fetch_data(self):
-        data = yf.download(self.ticker, start="2023-01-16", end="2024-01-16")
+        data = yf.download(self.ticker, start="2023-01-31", end="2024-01-31")
         return data
 
     def structure_low_index_pointer(self, length):
@@ -40,12 +41,36 @@ class OrderBlocksIndicator:
         structure_low_index = 0
         structure_low = 1000000
 
+        last_month = None  # Додаткова змінна для збереження останнього місяця
+
         for i in range(len(self.df)):
-            if self.df['Low'][i] < structure_low:
-                structure_low = self.df['Low'][i]
+            if self.df['Low'].iloc[i] < structure_low:
+                structure_low = self.df['Low'].iloc[i]
                 structure_low_index = self.structure_low_index_pointer(self.candle_range)
 
-            if self.df['Low'][i] < structure_low:
+            if self.df.index[i].month != last_month:
+                # Якщо місяць змінився, виводимо новий місяць
+                date_text = self.df.index[i].strftime('%B %Y')
+                self.fig.add_annotation(
+                    go.layout.Annotation(
+                        text=date_text,
+                        x=i,
+                        y=0,
+                        showarrow=False,
+                        font=dict(size=12, color='black'),
+                        xref="x",
+                        yref="paper",
+                        xanchor="center",
+                        yanchor="top"
+                    )
+                )
+
+                # Оновлення вісі x для прибирання цифр
+                self.fig.update_xaxes(tickmode='array', tickvals=[], ticktext=[])
+
+                last_month = self.df.index[i].month
+
+            if self.df['Low'].iloc[i] < structure_low:
                 if (i - last_up_index) < 1000:
                     short_boxes.append(dict(x0=last_up_index, x1=last_up_index, y0=last_high, y1=last_up_low,
                                             fillcolor='rgba(255,0,0, 0.9)', line=dict(color='rgba(255,0,0, 0.9)'),
@@ -64,7 +89,7 @@ class OrderBlocksIndicator:
                     box = short_boxes[j]
                     top = box['y1']
                     left = box['x0']
-                    if self.df['Close'][i] > top:
+                    if self.df['Close'].iloc[i] > top:
                         short_boxes.pop(j)
                         if (i - last_down_index) < 1000 and i > last_long_index:
                             long_boxes.append(dict(x0=last_down_index, x1=last_down_index, y0=last_down, y1=last_low,
@@ -77,48 +102,48 @@ class OrderBlocksIndicator:
 
                             self.plot_candle(i, CandleColourMode=1, show_bos_candle=True, CandleColour='rgba(0,255,0, 0.9)')
                             last_long_index = i
-                            last_bull_break_low = self.df['Low'][i]
+                            last_bull_break_low = self.df['Low'].iloc[i]
 
             if len(long_boxes) > 0:
                 for j in range(len(long_boxes) - 1, -1, -1):
                     lbox = long_boxes[j]
                     bottom = lbox['y1']
                     top = lbox['y0']
-                    if self.df['Close'][i] < bottom:
+                    if self.df['Close'].iloc[i] < bottom:
                         long_boxes.pop(j)
 
-            self.plot_candle(i, CandleColourMode=1 if self.df['Close'][i] > self.df['Open'][i] else 0,
+            self.plot_candle(i, CandleColourMode=1 if self.df['Close'].iloc[i] > self.df['Open'].iloc[i] else 0,
                              show_bos_candle=False)
 
-            if self.df['Close'][i] < self.df['Open'][i]:
-                last_down = self.df['High'][i]
+            if self.df['Close'].iloc[i] < self.df['Open'].iloc[i]:
+                last_down = self.df['High'].iloc[i]
                 last_down_index = i
-                last_low = self.df['Low'][i]
+                last_low = self.df['Low'].iloc[i]
 
-            if self.df['Close'][i] > self.df['Open'][i]:
-                last_up = self.df['Close'][i]
+            if self.df['Close'].iloc[i] > self.df['Open'].iloc[i]:
+                last_up = self.df['Close'].iloc[i]
                 last_up_index = i
-                last_up_open = self.df['Open'][i]
-                last_up_low = self.df['Low'][i]
-                last_high = self.df['High'][i]
+                last_up_open = self.df['Open'].iloc[i]
+                last_up_low = self.df['Low'].iloc[i]
+                last_high = self.df['High'].iloc[i]
 
-            last_high = max(self.df['High'][i], last_high)
-            last_low = min(self.df['Low'][i], last_low)
+            last_high = max(self.df['High'].iloc[i], last_high)
+            last_low = min(self.df['Low'].iloc[i], last_low)
 
         self.fig.show()
 
-    def plot_candle(self, i, CandleColourMode, show_bos_candle=False):
+    def plot_candle(self, i, CandleColourMode, show_bos_candle=False, CandleColour='rgba(0,0,0, 0.9)'):
         candle = go.Candlestick(
             x=[i],
-            open=[self.df['Open'][i]],
-            high=[self.df['High'][i]],
-            low=[self.df['Low'][i]],
-            close=[self.df['Close'][i]],
+            open=[self.df['Open'].iloc[i]],
+            high=[self.df['High'].iloc[i]],
+            low=[self.df['Low'].iloc[i]],
+            close=[self.df['Close'].iloc[i]],
             hoverinfo='x+y+z',
             increasing_line_color='green',
             decreasing_line_color='red',
-            increasing_fillcolor='rgba(0,255,0, 0.9)',  # Змінено fillcolor на increasing_fillcolor
-            decreasing_fillcolor='rgba(255,0,0, 0.9)'  # Змінено fillcolor на decreasing_fillcolor
+            increasing_fillcolor=CandleColour,
+            decreasing_fillcolor=CandleColour
         )
 
         self.fig.add_trace(candle)
@@ -127,22 +152,21 @@ class OrderBlocksIndicator:
             self.fig.add_trace(
                 go.Candlestick(
                     x=[i],
-                    open=[self.df['Open'][i]],
-                    high=[self.df['High'][i]],
-                    low=[self.df['Low'][i]],
-                    close=[self.df['Close'][i]],
+                    open=[self.df['Open'].iloc[i]],
+                    high=[self.df['High'].iloc[i]],
+                    low=[self.df['Low'].iloc[i]],
+                    close=[self.df['Close'].iloc[i]],
                     hoverinfo='x+y+z',
                     increasing_line_color='green',
                     decreasing_line_color='red',
-                    increasing_fillcolor='rgba(0,255,0, 0.9)',
-                    decreasing_fillcolor='rgba(255,0,0, 0.9)'
+                    increasing_fillcolor=CandleColour,
+                    decreasing_fillcolor=CandleColour
                 )
             )
 
 
-
-
+# Example usage:
 if __name__ == "__main__":
-    indicator = OrderBlocksIndicator(ticker="AAPL", candle_range=15, show_pd=False, show_bearish_bos=False,
+    indicator = OrderBlocksIndicator(ticker="BTC-USD", candle_range=15, show_pd=False, show_bearish_bos=False,
                                      show_bullish_bos=False)
     indicator.plot_order_blocks()
